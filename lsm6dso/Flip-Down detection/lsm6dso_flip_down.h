567b9b7dcf4b0b04e4dc18f1ef9e859301da169e
Stefano Paolo Rivolta <stefano-paolo.rivolta@st.com>
2019-07-01 18:30:51 +0200

    lsm6dso: added example Flip-Down detection
    Signed-off-by: Stefano Paolo Rivolta <stefano-paolo.rivolta@st.com>

---
+++ lsm6dso/Flip-Down detection/lsm6dso_flip_down.h
@@ -0,0 +1,123 @@
+/*
+ ******************************************************************************
+ * @file    lsm6dso_flip_down.h
+ * @author  Sensors Software Solution Team
+ * @brief   This file contains the configuration for lsm6dso_flip_down.
+ *
+ ******************************************************************************
+ * @attention
+ *
+ * <h2><center>&copy; COPYRIGHT(c) 2019 STMicroelectronics</center></h2>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *   1. Redistributions of source code must retain the above copyright notice,
+ *      this list of conditions and the following disclaimer.
+ *   2. Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in the
+ *      documentation and/or other materials provided with the distribution.
+ *   3. Neither the name of STMicroelectronics nor the names of its
+ *      contributors may be used to endorse or promote products derived from
+ *      this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+ 
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef LSM6DSO_FLIP_DOWN_H
+#define LSM6DSO_FLIP_DOWN_H
+
+#ifdef __cplusplus
+  extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include <stdint.h>
+
+#ifndef MEMS_UCF_SHARED_TYPES
+#define MEMS_UCF_SHARED_TYPES
+
+/** Common data block definition **/
+typedef struct {
+  uint8_t address;
+  uint8_t data;
+} ucf_line_t;
+
+#endif /* MEMS_UCF_SHARED_TYPES */
+
+/** Configuration array generated from Unico Tool **/
+const ucf_line_t lsm6dso_flip_down[] = {
+  {.address = 0x01, .data = 0x80,},
+  {.address = 0x04, .data = 0x00,},
+  {.address = 0x05, .data = 0x00,},
+  {.address = 0x5F, .data = 0x0A,},
+  {.address = 0x46, .data = 0x01,},
+  {.address = 0x47, .data = 0x00,},
+  {.address = 0x0A, .data = 0x00,},
+  {.address = 0x0B, .data = 0x01,},
+  {.address = 0x0C, .data = 0x00,},
+  {.address = 0x0E, .data = 0x00,},
+  {.address = 0x0F, .data = 0x00,},
+  {.address = 0x10, .data = 0x00,},
+  {.address = 0x02, .data = 0x01,},
+  {.address = 0x17, .data = 0x40,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x02, .data = 0x11,},
+  {.address = 0x08, .data = 0x7A,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x09, .data = 0x01,},
+  {.address = 0x09, .data = 0x01,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x09, .data = 0x04,},
+  {.address = 0x02, .data = 0x41,},
+  {.address = 0x08, .data = 0x00,},
+  {.address = 0x09, .data = 0x92,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x09, .data = 0x16,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x09, .data = 0x0F,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x09, .data = 0xCD,},
+  {.address = 0x09, .data = 0xBA,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x09, .data = 0x08,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x09, .data = 0x0A,},
+  {.address = 0x09, .data = 0x0D,},
+  {.address = 0x09, .data = 0x06,},
+  {.address = 0x09, .data = 0x84,},
+  {.address = 0x09, .data = 0x33,},
+  {.address = 0x09, .data = 0x53,},
+  {.address = 0x09, .data = 0x44,},
+  {.address = 0x09, .data = 0x22,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x04, .data = 0x00,},
+  {.address = 0x05, .data = 0x01,},
+  {.address = 0x17, .data = 0x00,},
+  {.address = 0x02, .data = 0x01,},
+  {.address = 0x01, .data = 0x00,},
+  {.address = 0x14, .data = 0x80,},
+  {.address = 0x10, .data = 0x28,},
+  {.address = 0x11, .data = 0x00,},
+  {.address = 0x5E, .data = 0x02,}
+};
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* LSM6DSO_FLIP_DOWN_H */
