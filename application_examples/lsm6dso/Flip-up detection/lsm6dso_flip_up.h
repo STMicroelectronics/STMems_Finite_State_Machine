a8fa80812c703ae1d19fa8afd2f6bf6349658127
Stefano Paolo Rivolta <stefano-paolo.rivolta@st.com>
2020-12-16 17:40:59 +0100

    lsm6dso: added application example flip-up detection
    Signed-off-by: Stefano Paolo Rivolta <stefano-paolo.rivolta@st.com>

---
+++ application_examples/lsm6dso/Flip-up detection/lsm6dso_flip_up.h
@@ -0,0 +1,123 @@
+/*
+ ******************************************************************************
+ * @file    lsm6dso_flip_up.h
+ * @author  Sensors Software Solution Team
+ * @brief   This file contains the configuration for lsm6dso_flip_up.
+ *
+ ******************************************************************************
+ * @attention
+ *
+ * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
+ * All rights reserved.</center></h2>
+ *
+ * This software component is licensed by ST under BSD 3-Clause license,
+ * the "License"; You may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at:
+ *                        opensource.org/licenses/BSD-3-Clause
+ *
+ */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef LSM6DSO_FLIP_UP_H
+#define LSM6DSO_FLIP_UP_H
+
+#ifdef __cplusplus
+  extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include <stdint.h>
+#ifndef MEMS_UCF_SHARED_TYPES
+#define MEMS_UCF_SHARED_TYPES
+
+/** Common data block definition **/
+typedef struct {
+  uint8_t address;
+  uint8_t data;
+} ucf_line_t;
+
+#endif /* MEMS_UCF_SHARED_TYPES */
+
+/** Configuration array generated from Unico Tool **/
+const ucf_line_t lsm6dso_flip_up[] = {
+  {.address = 0x10, .data = 0x00,},
+  {.address = 0x11, .data = 0x00,},
+  {.address = 0x01, .data = 0x80,},
+  {.address = 0x04, .data = 0x00,},
+  {.address = 0x05, .data = 0x00,},
+  {.address = 0x5F, .data = 0x4B,},
+  {.address = 0x46, .data = 0x01,},
+  {.address = 0x47, .data = 0x00,},
+  {.address = 0x0A, .data = 0x00,},
+  {.address = 0x0B, .data = 0x01,},
+  {.address = 0x0C, .data = 0x00,},
+  {.address = 0x0E, .data = 0x00,},
+  {.address = 0x0F, .data = 0x00,},
+  {.address = 0x10, .data = 0x00,},
+  {.address = 0x17, .data = 0x40,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x02, .data = 0x11,},
+  {.address = 0x08, .data = 0x7A,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x09, .data = 0x01,},
+  {.address = 0x09, .data = 0x01,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x09, .data = 0x04,},
+  {.address = 0x02, .data = 0x41,},
+  {.address = 0x08, .data = 0x00,},
+  {.address = 0x09, .data = 0xE2,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x09, .data = 0x28,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x09, .data = 0x13,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x09, .data = 0x33,},
+  {.address = 0x09, .data = 0xBB,},
+  {.address = 0x09, .data = 0xCD,},
+  {.address = 0x09, .data = 0x34,},
+  {.address = 0x09, .data = 0x66,},
+  {.address = 0x09, .data = 0x3A,},
+  {.address = 0x09, .data = 0x08,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x09, .data = 0x80,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x09, .data = 0x15,},
+  {.address = 0x09, .data = 0x68,},
+  {.address = 0x09, .data = 0x13,},
+  {.address = 0x09, .data = 0x66,},
+  {.address = 0x09, .data = 0xCC,},
+  {.address = 0x09, .data = 0x53,},
+  {.address = 0x09, .data = 0xDD,},
+  {.address = 0x09, .data = 0x13,},
+  {.address = 0x09, .data = 0x66,},
+  {.address = 0x09, .data = 0x41,},
+  {.address = 0x09, .data = 0x68,},
+  {.address = 0x09, .data = 0x13,},
+  {.address = 0x09, .data = 0x1E,},
+  {.address = 0x09, .data = 0x12,},
+  {.address = 0x09, .data = 0x77,},
+  {.address = 0x09, .data = 0x41,},
+  {.address = 0x09, .data = 0x75,},
+  {.address = 0x09, .data = 0x18,},
+  {.address = 0x09, .data = 0x24,},
+  {.address = 0x09, .data = 0x13,},
+  {.address = 0x09, .data = 0x66,},
+  {.address = 0x09, .data = 0x46,},
+  {.address = 0x09, .data = 0x22,},
+  {.address = 0x04, .data = 0x00,},
+  {.address = 0x05, .data = 0x01,},
+  {.address = 0x17, .data = 0x00,},
+  {.address = 0x01, .data = 0x00,},
+  {.address = 0x14, .data = 0x80,},
+  {.address = 0x5E, .data = 0x02,},
+  {.address = 0x10, .data = 0x28,}
+};
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* LSM6DSO_FLIP_UP_H */
+
