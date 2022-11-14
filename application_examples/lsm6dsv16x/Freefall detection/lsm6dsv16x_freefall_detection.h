03e0a1bf4d2b3cca1ab10dab486d74f558e63947
Stefano Paolo Rivolta <stefano-paolo.rivolta@st.com>
2022-11-14 11:15:45 +0100

    lsm6dsv16x: added application example Freefall detection
    Signed-off-by: Stefano Paolo Rivolta <stefano-paolo.rivolta@st.com>

---
+++ application_examples/lsm6dsv16x/Freefall detection/lsm6dsv16x_freefall_detection.h
@@ -0,0 +1,107 @@
+/*
+ ******************************************************************************
+ * @file    lsm6dsv16x_freefall_detection.h
+ * @author  MEMS Software Solutions Team
+ * @brief   This file contains the configuration for lsm6dsv16x_freefall_detection.
+ *
+ ******************************************************************************
+ * @attention
+ *
+ * <h2><center>&copy; Copyright (c) 2022 STMicroelectronics.
+ * All rights reserved.</center></h2>
+ *
+ * This software component is licensed by ST under BSD 3-Clause license,
+ * the "License"; You may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at:
+ *                        opensource.org/licenses/BSD-3-Clause
+ *
+ */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef LSM6DSV16X_FREEFALL_DETECTION_H
+#define LSM6DSV16X_FREEFALL_DETECTION_H
+
+#ifdef __cplusplus
+  extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include <stdint.h>
+#ifndef MEMS_UCF_SHARED_TYPES
+#define MEMS_UCF_SHARED_TYPES
+
+/** Common data block definition **/
+typedef struct {
+  uint8_t address;
+  uint8_t data;
+} ucf_line_t;
+
+#endif /* MEMS_UCF_SHARED_TYPES */
+
+/** Configuration array generated from Unico Tool **/
+const ucf_line_t lsm6dsv16x_freefall_detection[] = {
+  {.address = 0x10, .data = 0x00,},
+  {.address = 0x11, .data = 0x00,},
+  {.address = 0x01, .data = 0x80,},
+  {.address = 0x04, .data = 0x00,},
+  {.address = 0x05, .data = 0x00,},
+  {.address = 0x5F, .data = 0x4B,},
+  {.address = 0x46, .data = 0x01,},
+  {.address = 0x0A, .data = 0x00,},
+  {.address = 0x0B, .data = 0x01,},
+  {.address = 0x0E, .data = 0x00,},
+  {.address = 0x0F, .data = 0x00,},
+  {.address = 0x17, .data = 0x40,},
+  {.address = 0x02, .data = 0x11,},
+  {.address = 0x08, .data = 0x7A,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x09, .data = 0x01,},
+  {.address = 0x09, .data = 0x01,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x09, .data = 0x04,},
+  {.address = 0x02, .data = 0x41,},
+  {.address = 0x08, .data = 0x00,},
+  {.address = 0x09, .data = 0x71,},
+  {.address = 0x09, .data = 0x10,},
+  {.address = 0x09, .data = 0x1C,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x09, .data = 0x11,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x09, .data = 0xCD,},
+  {.address = 0x09, .data = 0x34,},
+  {.address = 0x09, .data = 0xA8,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x09, .data = 0x01,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x09, .data = 0x04,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x09, .data = 0xF5,},
+  {.address = 0x09, .data = 0x77,},
+  {.address = 0x09, .data = 0x99,},
+  {.address = 0x09, .data = 0x12,},
+  {.address = 0x09, .data = 0x66,},
+  {.address = 0x09, .data = 0x53,},
+  {.address = 0x09, .data = 0xC7,},
+  {.address = 0x09, .data = 0x88,},
+  {.address = 0x09, .data = 0x99,},
+  {.address = 0x09, .data = 0x50,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x04, .data = 0x00,},
+  {.address = 0x05, .data = 0x01,},
+  {.address = 0x17, .data = 0x00,},
+  {.address = 0x01, .data = 0x00,},
+  {.address = 0x5E, .data = 0x02,},
+  {.address = 0x17, .data = 0x03,},
+  {.address = 0x10, .data = 0x44,}
+};
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* LSM6DSV16X_FREEFALL_DETECTION_H */
+
